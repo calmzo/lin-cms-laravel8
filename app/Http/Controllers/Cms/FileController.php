acb82958471eb3da363f0b6f3b608c74a48b4a0f
Calm <calmchen@primecare.group>
2022-08-25 16:02:45 +0800

    上传文件

--- app/Http/Controllers/Cms/FileController.php
+++ app/Http/Controllers/Cms/FileController.php
@@ -4,27 +4,24 @@ namespace App\Http\Controllers\Cms;
 
 use App\Exceptions\File\FileException;
 use App\Lib\UploadService;
-use App\Models\Admin\LinFile;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 /**
- * Class File
- * @package app\api\controller\cms
+ *
  */
 class FileController extends BaseController
 {
     protected $only = [];
 
     /**
-     * @return mixed
+     *
+     * @param Request $request
+     * @return array
      * @throws FileException
-     * @throws \LinCmsTp\exception\FileException
      */
     public function postFile(Request $request)
     {
-        if(!$request->hasFile('file'))
-        {
+        if (!$request->hasFile('file')) {
             throw new FileException();
         }
         return UploadService::upload($request->file('file'));
