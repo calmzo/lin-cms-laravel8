e2095d0eaa6a8bbeb340da3413f5d97f90c75a7c
chenzhuo <178320938@qq.com>
2022-09-03 16:08:17 +0800

    任务表优化

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Task extends BaseModel
 {
     use BooleanSoftDeletes, HasFactory;
+
+    public function order()
+    {
+        return $this->hasOne(Order::class,'id', 'item_id');
+    }
+
+    public function trade()
+    {
+        return $this->hasOne(Trade::class,'id', 'item_id');
+    }
+
+    public function refund()
+    {
+        return $this->hasOne(Refund::class,'id', 'item_id');
+    }
 }
--- database/migrations/2022_09_02_101036_create_task.php
+++ database/migrations/2022_09_02_101036_create_task.php
@@ -17,11 +17,11 @@ class CreateTask extends Migration
             $table->id();
             $table->integer('item_id')->default(0)->comment('条目id');
             $table->integer('item_type')->default(0)->comment('条目类型 1=发货 2=退款');
-            $table->string('item_info', 3000)->default('条目内容');
+            $table->string('item_info', 3000)->default('')->comment('条目内容');
             $table->integer('status')->default(1)->comment('状态 1=待定 2=完成 3=取消 4=失败');
             $table->integer('priority')->default(30)->comment('优先级 10=高 20=中 30=低');
             $table->integer('try_count')->default(0)->comment('重试数');
-            $table->integer('max_try_count')->default(0)->comment('最大重试数');
+            $table->integer('max_try_count')->default(3)->comment('最大重试数');
             $table->timestamp('create_time');
             $table->timestamp('update_time');
             $table->softDeletes('delete_time');
