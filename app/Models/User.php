e1446653c20b2517965e2ab2163752b9cf680e41
chenzhuo <178320938@qq.com>
2022-09-04 23:46:43 +0800

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,42 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends BaseModel implements JWTSubject, AuthenticatableContract,
+    AuthorizableContract
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Authenticatable, Authorizable, BooleanSoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    public $fillable = [
+        ''
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTCustomClaims()
+    {
+        return [
+            'issuer' => env('JWT_ISSUER'),
+            'userId' => $this->getKey(),
+            'permissions' => '',
+            'admin' => '',
+        ];    }
 }
