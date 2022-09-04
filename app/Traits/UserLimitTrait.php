405bd2ad595e2b2b4252ed616de765f13b7cc8c7
chenzhuo <178320938@qq.com>
2022-09-04 23:42:44 +0800

    userLimitTrait

---
+++ app/Traits/UserLimitTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Traits;
+
+use App\Caches\UserDailyCounter;
+use App\Exceptions\BadRequestException;
+use App\User;
+use App\Utils\CodeResponse;
+
+trait UserLimitTrait
+{
+
+    protected $counter;
+
+    public function __construct()
+    {
+        $this->counter = new UserDailyCounter();
+    }
+
+
+    public function checkDailyOrderLimit($userId)
+    {
+        $count = $this->counter->hGet($userId, 'order_count');
+
+        if ($count > 50) {
+            throw new BadRequestException(CodeResponse::NOT_FOUND_EXCEPTION, 'user_limit.reach_daily_order_limit');
+        }
+    }
+
+
+
+}
