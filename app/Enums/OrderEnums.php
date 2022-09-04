989e652cf95a92ea818432a2f8ec9cb6ffad74f9
chenzhuo <178320938@qq.com>
2022-09-04 23:41:59 +0800

    条目类型枚举

--- app/Enums/OrderEnums.php
+++ app/Enums/OrderEnums.php
@@ -13,6 +13,17 @@ class OrderEnums
     const ITEM_VIP = 4; // 会员
     const ITEM_TEST = 99; // 测试
 
+    public static function itemTypes()
+    {
+        return [
+            self::ITEM_COURSE => '课程',
+            self::ITEM_PACKAGE => '套餐',
+            self::ITEM_REWARD => '赞赏',
+            self::ITEM_VIP => '会员',
+            self::ITEM_TEST => '测试',
+        ];
+    }
+
     const PROMOTION_FLASH_SALE = 1; // 限时秒杀
     const PROMOTION_DISCOUNT = 2; // 限时折扣
 
