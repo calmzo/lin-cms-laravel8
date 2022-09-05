ed6a73fd2d07911ae3310ddc2cafb8db569b6dbc
Calm <calmchen@primecare.group>
2022-09-05 10:49:40 +0800

    获取客户端ip 类型

--- app/Traits/ClientTrait.php
+++ app/Traits/ClientTrait.php
@@ -5,9 +5,21 @@ namespace App\Traits;
 use App\Enums\ClientEnums;
 use App\Exceptions\BadRequestException;
 use App\Utils\CodeResponse;
+use hisorange\BrowserDetect\Parser;
+use Browser;
 
 trait ClientTrait
 {
+
+    /**
+     * 获取客户端ip
+     * @return mixed|string|null
+     */
+    public function getClientIp()
+    {
+        return request()->ip();
+    }
+
     public function checkH5Platform($platform)
     {
         $platform = strtoupper($platform);
@@ -43,4 +55,26 @@ trait ClientTrait
         throw new BadRequestException(CodeResponse::NOT_FOUND_EXCEPTION, '无效的终端类型');
     }
 
+    /**
+     * 获取客户端类型
+     * @return int|mixed|string
+     */
+    public function getClientType()
+    {
+
+        $request = request();
+        $platform = $request->header('X-Platform');
+        $types = array_flip(ClientEnums::types());
+
+        if (!empty($platform) && isset($types[$platform])) {
+            return $types[$platform];
+        }
+        $clientType = ClientEnums::TYPE_PC;
+        // Determine the user's device type is simple as this:
+        if (Browser::isMobile()) {
+            $clientType = ClientEnums::TYPE_H5;
+        }
+
+        return $clientType;
+    }
 }
