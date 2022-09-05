9bc80a0a80a85ab8a1b695dfc1def3b78cc6f3f3
Calm <calmchen@primecare.group>
2022-09-05 17:36:14 +0800

    用户账户表 account migration + model

---
+++ database/migrations/2022_09_05_085950_create_user_balance.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserBalance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_balance', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->default(0)->comment('用户id');
+            $table->decimal('cash', 10)->default(0.00)->comment('可用现金');
+            $table->integer('point')->default(0)->comment('可用积分');
+            $table->timestamp('create_time');
+            $table->timestamp('update_time');
+            $table->softDeletes('delete_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_balance');
+    }
+}
