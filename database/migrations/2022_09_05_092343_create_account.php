d5d38d5e39ddfdf0f95c904ad4fb49452803737b
Calm <calmchen@primecare.group>
2022-09-05 17:36:28 +0800

    用户账户表 account migration + model

---
+++ database/migrations/2022_09_05_092343_create_account.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccount extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account', function (Blueprint $table) {
+            $table->id();
+            $table->string('email', 24)->default('')->comment('邮箱');
+            $table->string('phone', 24)->default('')->comment('手机号');
+            $table->string('password', 500)->default('')->comment('密码');
+            $table->timestamp('create_time');
+            $table->timestamp('update_time');
+            $table->softDeletes('delete_time');
+            $table->index('phone');
+            $table->index('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account');
+    }
+}
