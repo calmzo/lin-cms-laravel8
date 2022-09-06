4a2c822898846e004d1591a792138f8193355f97
Calm <calmchen@primecare.group>
2022-09-06 10:42:36 +0800

    migration: create_wechat_subscribe

---
+++ database/migrations/2022_09_06_012744_create_wechat_subscribe.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWechatSubscribe extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wechat_subscribe', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->default(0)->comment('用户id');
+            $table->string('open_id', 64)->default('')->comment('开放ID');
+            $table->string('union_id', 64)->default('')->comment('联合ID');
+            $table->timestamp('create_time');
+            $table->timestamp('update_time');
+            $table->softDeletes('delete_time');
+            $table->index('user_id', 'user_id');
+            $table->index('open_id', 'phone', 'BTREE');
+            $table->index('union_id', 'union_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wechat_subscribe');
+    }
+}
