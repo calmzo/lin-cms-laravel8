774fccfeb08f1f4eada22eb057a4b828e68245ae
Calm <calmchen@primecare.group>
2022-09-06 10:41:23 +0800

    微信模板配置： 订单完成 退款 咨询回复

--- config/wechat.php
+++ config/wechat.php
@@ -8,19 +8,32 @@ return [
         'app_secret' =>  env('WECHAT_OA_APP_SECRET') ?? '',
         'app_token' =>  env('WECHAT_OA_APP_TOKEN') ?? '',
         'aes_key' =>  env('WECHAT_OA_AES_KEY') ?? '',
-
-        //开关
         'enabled' => 1,
         //通知模板
         'notice_template' => [
             //账号登录
             'account_login' => [
-                //开关
                 'enabled' => 1,
-                //模板id
                 'id' => 1234
             ],
-        ]
+            //订单完成
+            'order_finish' => [
+                'enabled' => 1,
+                'id' => 1234
+            ],
+            //退款
+            'refund_finish' => [
+                'enabled' => 1,
+                'id' => 1234
+            ],
+            //咨询回复模板
+            'consult_reply' => [
+                'enabled' => 1,
+                'id' => 1234
+            ],
+        ],
+
+
     ],
 
 ];
