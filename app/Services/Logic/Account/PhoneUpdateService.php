df6590594aa5f00d3c6ec7b73c00c7a89b1a5ca1
Calm <calmchen@primecare.group>
2022-09-08 13:44:37 +0800

    手机号修改类

---
+++ app/Services/Logic/Account/PhoneUpdateService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services\Logic\Account;
+
+use App\Models\Account;
+use App\Repos\Account as AccountRepo;
+use App\Services\Logic\Service as LogicService;
+use App\Services\Token\AccountLoginTokenService;
+use App\Lib\Validators\AccountValidator;
+use App\Lib\Validators\VerifyValidator;
+
+class PhoneUpdateService
+{
+
+    public function handle($params)
+    {
+        $phone = $params['phone'] ?? '';
+        $loginPassword = $params['login_password'] ?? '';
+        $verifyCode = $params['verify_code'] ?? '';
+        $user = AccountLoginTokenService::user();
+        $account = Account::query()->find($user->id);
+
+        $accountValidator = new AccountValidator();
+
+        $phone = $accountValidator->checkPhone($params['phone']);
+
+        if ($phone != $account->phone) {
+            $accountValidator->checkIfPhoneTaken($phone);
+        }
+
+        $accountValidator->checkLoginPassword($account, $loginPassword);
+
+        $verifyValidator = new VerifyValidator();
+
+        $verifyValidator->checkCode($phone, $verifyCode);
+
+        $account->phone = $phone;
+
+        $account->save();
+
+        return $account;
+    }
+
+}
