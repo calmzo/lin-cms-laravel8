749d0a40722dd14ee84d02a78addd8c4a12af167
Calm <calmchen@primecare.group>
2022-09-08 13:44:58 +0800

    邮箱修改类

---
+++ app/Services/Logic/Account/EmailUpdateService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services\Logic\Account;
+
+use App\Models\Account;
+use App\Services\Token\AccountLoginTokenService;
+use App\Lib\Validators\AccountValidator;
+use App\Lib\Validators\VerifyValidator;
+
+class EmailUpdateService
+{
+
+    public function handle($params)
+    {
+        $email = $params['email'] ?? '';
+        $loginPassword = $params['login_password'] ?? '';
+        $verifyCode = $params['verify_code'] ?? '';
+        $user = AccountLoginTokenService::user();
+
+        $account = Account::query()->find($user->id);
+
+        $accountValidator = new AccountValidator();
+
+        $email = $accountValidator->checkEmail($email);
+
+        if ($email != $account->email) {
+            $accountValidator->checkIfEmailTaken($email);
+        }
+
+        $accountValidator->checkLoginPassword($account, $loginPassword);
+
+        $verifyValidator = new VerifyValidator();
+
+        $verifyValidator->checkCode($email, $verifyCode);
+
+        $account->email = $email;
+
+        $account->save();
+
+        return $account;
+    }
+
+}
