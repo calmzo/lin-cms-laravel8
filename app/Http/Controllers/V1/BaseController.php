d4ee20a425752a8ee57b62b0fde231f363d957e4
Calm <calmchen@primecare.group>
2022-09-08 11:41:08 +0800

    api控制器基类

---
+++ app/Http/Controllers/V1/BaseController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Utils\CodeResponse;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
+
+class BaseController extends Controller
+{
+    protected $only;
+    protected $except;
+
+    /**
+     * WxController constructor.
+     */
+    public function __construct()
+    {
+        $options = [];
+        if (!is_null($this->only)) {
+            $options['only'] = $this->only;
+        }
+        if (!is_null($this->except)) {
+            $options['except'] = $this->except;
+        }
+//        $this->middleware(['auth:v1'], $options);
+    }
+
+    protected function codeReturn(array $codeResponse, $data = null, $info = '')
+    {
+        list($code, $message) = $codeResponse;
+        $ret = ['code' => $code];
+        if (!is_null($data)) {
+            $ret['result'] = $data;
+        }
+        $ret['message'] = $info ?: $message;
+        return response()->json($ret);
+    }
+
+    protected function success($data = null, $info = '成功')
+    {
+        list($code, $message) = CodeResponse::SUCCESS;
+        $ret = ['code' => $code];
+        if (!is_null($data)) {
+            $ret['result'] = $data;
+        }
+        $ret['message'] = $info ?: $message;
+        return response()->json($ret);
+    }
+
+    protected function fail(array $codeResponse = CodeResponse::FAIL, $message = '')
+    {
+        return $this->codeReturn($codeResponse, null, $message);
+    }
+
+    protected function successPaginate($page)
+    {
+//        return $this->success($this->paginate($page));
+        return $this->paginate($page);
+    }
+
+    protected function paginate($page)
+    {
+        if ($page instanceof LengthAwarePaginator) {
+            return [
+                'total' => $page->total(),
+                'page' => $page->currentPage() - 1,
+                'count' => $page->perPage(),
+                'items' => $page->items()
+            ];
+        }
+        if ($page instanceof Collection) {
+            $page = $page->toArray();
+        }
+        if (!is_array($page)) {
+            return $page;
+        }
+        $total = count($page);
+        return [
+            'total' => $total,
+            'page' => 0,
+            'limit' => $total,
+            'items' => $page
+        ];
+
+        return $page;
+
+    }
+
+    public function user()
+    {
+        return Auth::guard('v1')->user();
+    }
+
+
+    public function isLogin()
+    {
+        return !is_null($this->user());
+    }
+
+    public function userId()
+    {
+        return $this->user()->getAuthIdentifier();
+    }
+}
