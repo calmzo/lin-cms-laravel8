2ace5c03fac39628675a7e0e92617190dc0ced61
Calm <calmchen@primecare.group>
2022-09-09 16:55:53 +0800

    解锁用户任务命令

---
+++ app/Console/Commands/UnlockUserTaskCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\UserEnums;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class UnlockUserTaskCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:unlock_user_task';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = $this->findUsers();
+
+        echo sprintf('pending users: %s', $users->count()) . PHP_EOL;
+
+        if ($users->count() == 0) return;
+
+        echo '------ start unlock user task ------' . PHP_EOL;
+//        foreach ($users as $user) {
+//            $user->save(['locked' => 0]);
+//        }
+        $userIds = $users->pluck('id');
+        User::query()->whereIn('id', $userIds)->update(['locked' => 0]);
+        echo '------ end unlock user task ------' . PHP_EOL;
+    }
+
+    /**
+     * 查找待解锁用户
+     *
+     * @param int $limit
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    protected function findUsers($limit = 1000)
+    {
+        $time = date('Y-m-d H:i:s', time() - 6 * 3600);
+
+        return User::query()
+            ->where('locked', 1)
+            ->where('lock_expiry_time', '<', $time)
+            ->limit($limit)
+            ->get();
+    }
+}
