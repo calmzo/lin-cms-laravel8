4b11122fb1914369db779bec324223fed1348f0f
Calm <calmchen@primecare.group>
2022-09-09 13:44:53 +0800

    关闭订单task

---
+++ app/Console/Commands/CloseOrderTaskCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\OrderEnums;
+use Illuminate\Console\Command;
+use App\Models\Order;
+
+class CloseOrderTaskCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:close_order';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $orders = $this->findOrders();
+
+        echo sprintf('pending orders: %s', $orders->count()) . PHP_EOL;
+
+        if ($orders->count() == 0) return;
+
+        echo '------ start close order task ------' . PHP_EOL;
+
+//        foreach ($orders as $order) {
+//            $order->status = OrderEnums::STATUS_CLOSED;
+//            $order->save();
+//        }
+        $orderIds = $orders->pluck('id');
+        Order::query()->whereIn('id', $orderIds)->update(['status' => OrderEnums::STATUS_CLOSED]);
+
+        echo '------ end close order task ------' . PHP_EOL;
+    }
+
+
+    /**
+     * 查找待关闭订单
+     *
+     * @param int $limit
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    protected function findOrders($limit = 1000)
+    {
+        $status = OrderEnums::STATUS_PENDING;
+        $time = time() - 12 * 3600;
+        $time = date('Y-m-d H:i:s', $time);
+        $type = 0;
+
+        return Order::query()
+            ->where('status', $status)
+            ->where('promotion_type', $type)
+            ->where('create_time', $time)
+            ->limit($limit)
+            ->get();
+    }
+}
