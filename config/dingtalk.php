ab2c77188e8fa1f584b3deca46e665c24075d974
Calm <calmchen@primecare.group>
2022-09-09 16:01:39 +0800

    钉钉机器人通知配置

--- config/dingtalk.php
+++ config/dingtalk.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 return [
     'robot' => [
         'enabled' => 1,
-        'ts_mobiles' => ['13153187435']
+        'app_secret' => env('DINGTALK_ROBOT_APP_SECRET') ?? '',
+        'app_token' => env('DINGTALK_ROBOT_APP_TOKEN') ?? '',
+        'ts_mobiles' => '13153187435,13153187434'
     ]
 ];
