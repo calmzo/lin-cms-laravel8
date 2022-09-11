0d8e2321c2b688a2e6012ea8280014b94f0ccbe3
chenzhuo <178320938@qq.com>
2022-09-12 07:24:28 +0800

    Task模型优化 item_info修改器 获取器

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,16 +10,33 @@ class Task extends BaseModel
 
     public function order()
     {
-        return $this->hasOne(Order::class,'id', 'item_id');
+        return $this->hasOne(Order::class, 'id', 'item_id');
     }
 
     public function trade()
     {
-        return $this->hasOne(Trade::class,'id', 'item_id');
+        return $this->hasOne(Trade::class, 'id', 'item_id');
     }
 
     public function refund()
     {
-        return $this->hasOne(Refund::class,'id', 'item_id');
+        return $this->hasOne(Refund::class, 'id', 'item_id');
+    }
+
+    public function setItemInfoAttribute($value)
+    {
+        if (is_array($value) || is_object($value)) {
+            $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+        }
+        $this->attributes['item_info'] = $value;
+    }
+
+    public function getItemInfoAttribute($value)
+    {
+        if (is_string($value)) {
+            $value = json_decode($value, true);
+        }
+        return $value;
+
     }
 }
