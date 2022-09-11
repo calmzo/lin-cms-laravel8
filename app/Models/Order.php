93de85ca0eec5449ee7cfafd1e353cca4ba5d224
chenzhuo <178320938@qq.com>
2022-09-12 07:25:17 +0800

    Order模型事件 saved添加order_status

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Order extends BaseModel
 {
     use BooleanSoftDeletes, HasFactory;
+
+
+
+    /**
+     * 模型的 "booted" 方法
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        //处理 Order「saved」事件
+        static::saved(function ($order) {
+            //更新订单状态
+            if ($order->isDirty('status')) {
+                $orderStatus = new OrderStatus();
+                $orderStatus->order_id = $order->getAttribute('id');
+                $orderStatus->status = $order->getAttribute('status');
+                $orderStatus->save();
+            }
+        });
+    }
 }
