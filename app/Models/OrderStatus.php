55831a1c9057d112db69097cadb78d0e7a402a6e
chenzhuo <178320938@qq.com>
2022-09-12 07:26:06 +0800

    OrderStatus模型

---
+++ app/Models/OrderStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class OrderStatus extends BaseModel
+{
+    use HasFactory;
+
+    const UPDATED_AT = NULL;
+
+}
