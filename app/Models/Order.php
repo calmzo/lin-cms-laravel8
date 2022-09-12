9f10fef72ad0a52999230c55424ba091ac9f567a
chenzhuo <178320938@qq.com>
2022-09-12 08:58:40 +0800

    Order 模型事件 修改器 获取器

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,10 @@ class Order extends BaseModel
 {
     use BooleanSoftDeletes, HasFactory;
 
+    public $fillable = [
+        'user_id', 'item_id', 'item_type', 'item_info', 'amount', 'subject', 'sn', 'promotion_id', 'promotion_type',
+        'promotion_info', 'client_type', 'client_ip', 'status'
+    ];
 
 
     /**
@@ -27,5 +31,51 @@ class Order extends BaseModel
                 $orderStatus->save();
             }
         });
+
+        //处理 Order「created」事件
+        static::created(function ($order) {
+            //更新订单状态
+            $orderStatus = new OrderStatus();
+            $orderStatus->order_id = $order->getAttribute('id');
+            $orderStatus->save();
+        });
+
+        //处理 Order「creating」事件
+        static::creating(function ($order) {
+            $order->sn = self::getOrderSn();
+        });
+    }
+
+    public function setItemInfoAttribute($value)
+    {
+        if (is_array($value) || is_object($value)) {
+            $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+        }
+        $this->attributes['item_info'] = $value;
+    }
+
+    public function getItemInfoAttribute($value)
+    {
+        if (is_string($value)) {
+            $value = json_decode($value, true);
+        }
+        return $value;
+    }
+
+
+    /**
+     * 生成不重复订单sn
+     * @return string|void
+     */
+    protected static function getOrderSn()
+    {
+        $sn = date('YmdHis') . rand(1000, 9999);
+
+        $order = Order::query()->where('sn', $sn)->first();
+        if (!$order) {
+            return $sn;
+        }
+
+        self::getOrderSn();
     }
 }
