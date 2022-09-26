6d9421d843159578eaf2356e4e05b350c331e093
Calm <calmchen@primecare.group>
2022-09-26 17:12:51 +0800

    laravel log

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 
 Route::get('/test', [\App\Http\Controllers\TestController::class, 'index']);
 
