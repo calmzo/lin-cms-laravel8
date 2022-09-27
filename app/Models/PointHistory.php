5757b0122ecfeb6a3facb13984d3aa5a9c11bec0
Calm <calmchen@primecare.group>
2022-09-27 14:48:16 +0800

    model event_info属性修改器

--- app/Models/PointHistory.php
+++ app/Models/PointHistory.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class PointHistory extends BaseModel
 {
     use BooleanSoftDeletes, HasFactory;
+
+
+    public function setEventInfoAttribute($value)
+    {
+        if (is_array($value) || is_object($value)) {
+            $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+        }
+        $this->attributes['event_info'] = $value;
+    }
+
+    public function getEventInfoAttribute($value)
+    {
+        if (is_string($value)) {
+            $value = json_decode($value, true);
+        }
+        return $value;
+    }
 }
