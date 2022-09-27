e714d951f134c2b501475c48b9135be029c3dd68
Calm <calmchen@primecare.group>
2022-09-27 14:44:16 +0800

    会员文章数量

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Enums\ArticleEnums;
+use App\Models\Article;
 use App\Models\User;
 
 class UserService
@@ -15,5 +17,10 @@ class UserService
     public function countUsers()
     {
         return User::query()->count();
+    }
+
+    public function countArticles($uid)
+    {
+        return Article::query()->where('user_id', $uid)->where('published', ArticleEnums::PUBLISH_APPROVED)->count();
     }
 }
