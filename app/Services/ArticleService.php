2cbb0236f0084deee3842cfc23e6a7fced6cb827
Calm <calmchen@primecare.group>
2022-09-27 14:55:19 +0800

    ArticleService.php

--- app/Services/ArticleService.php
+++ app/Services/ArticleService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Enums\ArticleEnums;
 use App\Models\Article;
+use App\Services\Logic\Article\ArticleCreateService;
 
 class ArticleService
 {
@@ -11,5 +12,73 @@ class ArticleService
     public function countArticles()
     {
         return Article::query()->where('published', ArticleEnums::PUBLISH_APPROVED)->count();
+    }
+
+
+    /**
+     * @param $page
+     * @param $count
+     * @param string|null $start
+     * @param string|null $end
+     * @param string|null $name
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getArticles($page, $count, string $start = null, string $end = null, string $name = null)
+    {
+        list($page, $count) = paginate($page, $count);
+        $query = Article::query();
+        if ($name) {
+            $query->where('name', 'like', '%' . $name . '%');
+        }
+        if ($start && $end) {
+            $query->whereBetween('create_time', [$start, $end]);
+        }
+        return $query->orderByDesc('create_time')->paginate($count, ['*'], 'page', $page);
+    }
+
+
+    /**
+     * @param int $page
+     * @param int $count
+     * @param string|null $start
+     * @param string|null $end
+     * @param string|null $name
+     * @param string|null $keyword
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function searchArticles(int    $page, int $count, string $start = null,
+                                      string $end = null, string $name = null, string $keyword = null)
+    {
+        list($page, $count) = paginate($page, $count);
+        $query = Article::query();
+        if ($name) {
+            $query->where('name', 'like', '%' . $name . '%');
+        }
+        if ($start && $end) {
+            $query->whereBetween('create_time', [$start, $end]);
+        }
+
+        if ($keyword) {
+            $query->where('alias', 'like', '%' . $keyword . '%');
+        }
+
+        $res = $query->orderByDesc('create_time')->paginate($count, ['*'], 'page', $page);
+        return $res;
+    }
+
+
+    public function createArticle(array $params): Article
+    {
+        $articleCreateService = new ArticleCreateService();
+        $article = $articleCreateService->handle($params);
+
+//        if ($article->published == ArticleEnums::PUBLISH_APPROVED) {
+//            $location = $this->url->get(['for' => 'home.article.show', 'id' => $article->id]);
+//            $msg = '发布文章成功';
+//        } else {
+//            $location = $this->url->get(['for' => 'home.uc.articles']);
+//            $msg = '创建文章成功，管理员审核后对外可见';
+//        }
+        return $article;
     }
 }
