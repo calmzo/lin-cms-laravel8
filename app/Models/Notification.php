40a201747a9a6fde72831d99173cea677367d940
Calm <calmchen@primecare.group>
2022-09-28 14:26:41 +0800

    通知表.类.枚举

---
+++ app/Models/Notification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Notification extends BaseModel
+{
+    use BooleanSoftDeletes, HasFactory;
+
+    public function setEventInfoAttribute($value)
+    {
+        if (is_array($value) || is_object($value)) {
+            $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+        }
+        $this->attributes['event_info'] = $value;
+    }
+
+    public function getEventInfoAttribute($value)
+    {
+        if (is_string($value)) {
+            $value = json_decode($value, true);
+        }
+        return $value;
+    }
+}
---
+++ database/migrations/2022_09_28_060758_create_notification.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotification extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->id();
+            $table->integer('sender_id')->unsigned()->default(0)->comment('发送方编号');
+            $table->integer('receiver_id')->unsigned()->default(0)->comment('接收方编号');
+            $table->integer('event_id')->unsigned()->default(0)->comment('事件编号');
+            $table->integer('event_type')->unsigned()->default(0)->comment('事件类型');
+            $table->string('event_info', 1500)->default('')->comment('事件内容');
+            $table->integer('viewed')->unsigned()->default(0)->comment('已读标识');
+            $table->timestamp('create_time');
+            $table->timestamp('update_time');
+            $table->softDeletes('delete_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notification');
+    }
+}
