8f7b1e48fbd9fa5e11af8c9d5240211392423126
Calm <calmchen@primecare.group>
2022-09-28 14:29:28 +0800

    文章关闭评论服务类

---
+++ app/Services/Logic/Article/ArticleCloseService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\Logic\Article;
+
+use App\Lib\Validators\ArticleValidator;
+use App\Services\Token\AccountLoginTokenService;
+
+class ArticleCloseService
+{
+
+    public function handle($id)
+    {
+        $validator = new ArticleValidator();
+        $article = $validator->checkArticle($id);
+
+        $user = AccountLoginTokenService::user();
+        $validator->checkOwner($user->id, $article->user_id);
+
+        $article->closed = $article->closed == 1 ? 0 : 1;
+
+        $article->save();
+
+        return $article;
+    }
+
+}
