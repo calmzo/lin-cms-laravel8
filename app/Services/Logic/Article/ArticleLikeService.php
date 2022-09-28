b1da075758a889f3758fb53af3174b2e79ebd592
Calm <calmchen@primecare.group>
2022-09-28 15:34:57 +0800

    用户点赞服务

---
+++ app/Services/Logic/Article/ArticleLikeService.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Services\Logic\Article;
+
+
+use App\Events\ArticleAfterLikeEvent;
+use App\Events\ArticleAfterUndoLikeEvent;
+use App\Events\IncrArticleLikeCountEvent;
+use App\Lib\Notice\ArticleLiked;
+use App\Lib\Validators\ArticleValidator;
+use App\Models\Article;
+use App\Models\User;
+use App\Services\Token\AccountLoginTokenService;
+use App\Traits\UserLimitTrait;
+use App\Models\ArticleLike;
+use App\Services\Logic\Point\History\ArticleLikedService;
+
+class ArticleLikeService
+{
+
+    use UserLimitTrait;
+
+    public function handle($id)
+    {
+        $validator = new ArticleValidator();
+        $article = $validator->checkArticle($id);
+
+        $user = AccountLoginTokenService::user();
+
+        $this->checkDailyArticleLikeLimit($user);
+
+
+        $articleLike = ArticleLike::query()->where('article_id', $article->id)->where('user_id', $user->id)->first();
+
+        $isFirstTime = true;
+
+        if (!$articleLike) {
+
+            $articleLike = new ArticleLike();
+            $articleLike->article_id = $article->id;
+            $articleLike->user_id = $user->id;
+
+            $articleLike->save();
+
+        } else {
+
+            $isFirstTime = false;
+
+            $articleLike->delete_time = is_null($articleLike->delete_time) ? now() : null;
+
+            $articleLike->save();
+        }
+
+        $this->incrUserDailyArticleLikeCount($user);
+
+
+        if (is_null($articleLike->delete_time)) {
+
+            $action = 'do';
+
+            $this->incrArticleLikeCount($article);
+
+            ArticleAfterLikeEvent::dispatch($article);
+
+        } else {
+
+            $action = 'undo';
+
+            $this->decrArticleLikeCount($article);
+            ArticleAfterUndoLikeEvent::dispatch($article);
+        }
+
+        $isOwner = $user->id == $article->user_id;
+
+        /**
+         * 仅首次点赞发送通知和奖励积分
+         */
+        if ($isFirstTime && !$isOwner) {
+            $this->handleArticleLikedNotice($article, $user);
+            $this->handleArticleLikedPoint($articleLike);
+        }
+
+        return [
+            'action' => $action,
+            'count' => $article->like_count,
+        ];
+    }
+
+    protected function incrArticleLikeCount(Article $article)
+    {
+        $article->like_count += 1;
+
+        $article->save();
+    }
+
+    protected function decrArticleLikeCount(Article $article)
+    {
+        if ($article->like_count > 0) {
+            $article->like_count -= 1;
+            $article->save();
+        }
+    }
+
+    protected function incrUserDailyArticleLikeCount(User $user)
+    {
+        IncrArticleLikeCountEvent::dispatch($user);
+    }
+
+    protected function handleArticleLikedNotice(Article $article, User $sender)
+    {
+        $notice = new ArticleLiked();
+
+        $notice->handle($article, $sender);
+    }
+
+    protected function handleArticleLikedPoint(ArticleLike $articleLike)
+    {
+        $service = new ArticleLikedService();
+
+        $service->handle($articleLike);
+    }
+
+}
