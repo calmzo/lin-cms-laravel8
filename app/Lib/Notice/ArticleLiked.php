c08842e1745dfa369b1514881c18b14fb49000a3
Calm <calmchen@primecare.group>
2022-09-28 15:34:27 +0800

    用户点赞后通知

---
+++ app/Lib/Notice/ArticleLiked.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Lib\Notice;
+
+use App\Enums\NotificationEnums;
+use App\Models\Article;
+use App\Models\Notification;
+use App\Models\User;
+
+class ArticleLiked
+{
+
+    public function handle(Article $article, User $sender)
+    {
+        $notification = new Notification();
+
+        $notification->sender_id = $sender->id;
+        $notification->receiver_id = $article->user_id;
+        $notification->event_id = $article->id;
+        $notification->event_type = NotificationEnums::TYPE_ARTICLE_LIKED;
+        $notification->event_info = [
+            'article' => ['id' => $article->id, 'title' => $article->title],
+        ];
+
+        $notification->save();
+    }
+
+}
