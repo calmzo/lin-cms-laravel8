aeee7d77eca1c1744a6a37a70c9a1a5638c29832
Calm <calmchen@primecare.group>
2022-09-28 21:42:52 +0800

    查看事件优化

--- app/Events/IncrReviewCountEvent.php
+++ app/Events/IncrReviewCountEvent.php
@@ -2,6 +2,7 @@
 
 namespace App\Events;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -14,13 +15,16 @@ class IncrReviewCountEvent
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $user;
+
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
+        $this->user = $user;
         //
     }
 
