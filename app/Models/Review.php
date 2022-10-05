a88a7b0d0a5d51ce91b01cdb91a5b3265041b16c
chenzhuo <178320938@qq.com>
2022-10-05 09:20:54 +0800

    review模型事件

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,9 +7,25 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Review extends BaseModel
 {
     use BooleanSoftDeletes, HasFactory;
+
     public $fillable = [
-        'client_type', 'client_ip', 'course_id', 'user_id', 'content', 'rating1', 'rating2', 'rating3', 'published'
+        'client_type', 'client_ip', 'course_id', 'user_id', 'content', 'rating', 'rating1', 'rating2', 'rating3', 'published'
     ];
+    protected static function booted()
+    {
+        //处理 Review「creating」事件
+        static::creating(function ($review) {
+            $review->rating = self::getAvgRating($review);
+        });
+    }
+
+
+    public static function getAvgRating($review)
+    {
+        $sumRating = $review->rating1 + $review->rating2 + $review->rating3;
+
+        return round($sumRating / 3, 2);
+    }
 }
 
 
