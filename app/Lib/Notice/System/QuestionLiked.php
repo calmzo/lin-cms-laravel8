ed7fa2156c35da0b10acbc98c27a059ad35e1e25
chenzhuo <178320938@qq.com>
2022-10-05 16:31:41 +0800

    问题点赞后通知

---
+++ app/Lib/Notice/System/QuestionLiked.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Lib\Notice\System;
+
+use App\Enums\NotificationEnums;
+use App\Models\Notification;
+use App\Models\Question;
+use App\Models\User;
+use App\Services\Logic\LogicService;
+
+class QuestionLiked extends LogicService
+{
+
+    public function handle(Question $question, User $sender)
+    {
+        $notification = new Notification();
+
+        $notification->sender_id = $sender->id;
+        $notification->receiver_id = $question->user_id;
+        $notification->event_id = $question->id;
+        $notification->event_type = NotificationEnums::TYPE_QUESTION_LIKED;
+        $notification->event_info = [
+            'question' => ['id' => $question->id, 'title' => $question->title],
+        ];
+
+        $notification->save();
+    }
+
+}
