137723ded8e50c285ee4a08512bfea31695e061c
chenzhuo <178320938@qq.com>
2022-10-07 10:03:37 +0800

    答案model类+事件

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,6 +2,22 @@
 
 namespace App\Models;
 
+use App\Caches\MaxAnswerIdCache;
+
 class Answer extends BaseModel
 {
+    public $fillable = [
+        'client_type', 'user_id', 'question_id', 'cover', 'summary', 'content', 'anonymous',
+        'accepted', 'published', 'client_ip', 'comment_count', 'like_count', 'report_count'
+    ];
+
+    protected static function booted()
+    {
+        //处理 Article「created」事件
+        static::created(function () {
+            $cache = new MaxAnswerIdCache();
+            $cache->rebuild();
+        });
+
+    }
 }
