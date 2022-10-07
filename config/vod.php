b11cfa5aaf05b80b84db0d63c90eccfb4eb78f91
chenzhuo <178320938@qq.com>
2022-10-07 14:10:32 +0800

    点播第三方配置

--- config/vod.php
+++ config/vod.php
@@ -1,7 +1,24 @@
 <?php
 
 return [
-    'secret_id' => env('VOD_SECRET_ID'),
-    'secret_key' => env('VOD_SECRET_KEY'),
+    'secret' => [
+        'secret_id' => env('VOD_SECRET_ID'),
+        'secret_key' => env('VOD_SECRET_KEY'),
+    ],
+
+    'storage_region' => 'ap-guangzhou',
+    'storage_type' => 'nearby',
+    'video_format' => 'hls',
+    'audio_format' => 'mp3',
+    'wmk_enabled' => '1',
+    'wmk_tpl_id' => '569493',
+    'protocol' => 'https',
+    'domain' => '',
+    'key_anti_key' => '',
+    'key_anti_expiry' => '10800',
+    'key_anti_ip_limit' => '',
+    'key_anti_enabled' => '1',
+    'video_quality' => '["hd","sd","fd"]',
+    'audio_quality' => '["sd"]',
 
 ];
