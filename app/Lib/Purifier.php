3dbb252be70db6683e3373fcfbf03dc301aa5724
chenzhuo <178320938@qq.com>
2022-10-07 10:05:10 +0800

    第三方清理html

---
+++ app/Lib/Purifier.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Lib;
+
+use HTMLPurifier;
+use HTMLPurifier_Config;
+
+class Purifier
+{
+
+    /**
+     * @var HTMLPurifier
+     */
+    protected $purifier;
+
+    /**
+     * @var array
+     */
+    protected $options = [
+        'CSS.AllowedProperties' => 'color,font-size,text-align,background-color',
+        'AutoFormat.AutoParagraph' => true,
+        'AutoFormat.RemoveEmpty' => true,
+        'HTML.TargetBlank' => true,
+    ];
+
+    public function __construct($options = [])
+    {
+        $options = array_merge($this->options, $options);
+        $config = $this->getConfig($options);
+        $this->purifier = new HTMLPurifier($config);
+    }
+
+    public function clean($html)
+    {
+        return $this->purifier->purify($html);
+    }
+
+    public function cleanArray(array $html)
+    {
+        return $this->purifier->purifyArray($html);
+    }
+
+    protected function getConfig(array $options)
+    {
+        $config = HTMLPurifier_Config::createDefault();
+        foreach ($options as $key => $value) {
+            $config->set($key, $value);
+        }
+
+        $serializerPath = cache_path('purifier');
+
+        if (file_exists($serializerPath) == false) {
+            mkdir($serializerPath, 0777);
+        }
+
+        $config->set('Cache.SerializerPath', $serializerPath);
+
+        return $config;
+    }
+
+}
