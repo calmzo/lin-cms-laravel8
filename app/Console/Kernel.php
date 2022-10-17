4c502e9cb1b9f1482f953c4e2bbe626af1ee2e67
Calm <calmchen@primecare.group>
2022-10-17 19:26:48 +0800

    定时任务优化

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -80,34 +80,34 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('deliver_task')->everyMinute();
-        $schedule->command('vod_event_task')->everyFiveMinutes();
-        $schedule->command('sync_learning_task')->cron('*/7 * * * *');
-        $schedule->command('teacher_live_notice_task')->cron('*/9 * * * *');
-        $schedule->command('point_gift_deliver_task')->cron('*/11 * * * *');
-        $schedule->command('server_monitor_task')->cron('*/12 * * * *');
-        $schedule->command('close_trade')->cron('*/12 * * * *');
-        $schedule->command('close_flash_sale_order')->everyFifteenMinutes();
-        $schedule->command('notice_task')->everyMinute();
-        $schedule->command('close_order')->everyThreeHours();
-        $schedule->command('refund_task')->cron('0 */7 * * *');
-        $schedule->command('sync_course_index_task')->cron('0 */11 * * *');
-        $schedule->command('sync_group_index_task')->cron('0 */17 * * *');
-        $schedule->command('sync_user_index_task')->cron('0 */23 * * *');
-        $schedule->command('sync_article_index_task')->cron('0 */27 * * *');
-        $schedule->command('sync_question_index_task')->cron('0 */29 * * *');
-        $schedule->command('sync_course_score_task')->cron('0 */31 * * *');
-        $schedule->command('sync_article_score_task')->cron('0 */33 * * *');
-        $schedule->command('sync_question_score_task')->cron('0 */37 * * *');
-        $schedule->command('clean_log_task')->dailyAt('03:03');
-        $schedule->command('unlock_user_task')->dailyAt('03:07');
-        $schedule->command('revoke_vip_task')->dailyAt('03:11');
-        $schedule->command('sync_app_info_task')->dailyAt('03:13');
-        $schedule->command('sync_tag_stat_task')->dailyAt('03:17');
-        $schedule->command('sync_course_stat_task')->dailyAt('03:19');
-        $schedule->command('close_question_task')->dailyAt('03:23');
-        $schedule->command('sitemap_task')->dailyAt('04:03');
-        $schedule->command('teacher_live_notice_task')->dailyAt('04:07');
+        $schedule->command('deliver_task')->everyMinute()->appendOutputTo( storage_path('logs/deliver_task.log'));
+        $schedule->command('vod_event_task')->everyFiveMinutes()->appendOutputTo( storage_path('logs/vod_event_task.log'));
+        $schedule->command('sync_learning_task')->cron('*/7 * * * *')->appendOutputTo( storage_path('logs/sync_learning_task.log'));
+        $schedule->command('teacher_live_notice_task')->cron('*/9 * * * *')->appendOutputTo( storage_path('logs/teacher_live_notice_task.log'));
+        $schedule->command('point_gift_deliver_task')->cron('*/11 * * * *')->appendOutputTo( storage_path('logs/point_gift_deliver_task.log'));
+        $schedule->command('server_monitor_task')->cron('*/12 * * * *')->appendOutputTo( storage_path('logs/server_monitor_task.log'));
+        $schedule->command('close_trade')->cron('*/12 * * * *')->appendOutputTo( storage_path('logs/close_trade.log'));
+        $schedule->command('close_flash_sale_order')->everyFifteenMinutes()->appendOutputTo( storage_path('logs/close_flash_sale_order.log'));
+        $schedule->command('notice_task')->everyMinute()->appendOutputTo( storage_path('logs/notice_task.log'));
+        $schedule->command('close_order')->everyThreeHours()->appendOutputTo( storage_path('logs/close_order.log'));
+        $schedule->command('refund_task')->cron('0 */7 * * *')->appendOutputTo( storage_path('logs/refund_task.log'));
+        $schedule->command('sync_course_index_task')->cron('0 */11 * * *')->appendOutputTo( storage_path('logs/sync_course_index_task.log'));
+//        $schedule->command('sync_group_index_task')->cron('0 */17 * * *')->appendOutputTo( storage_path('logs/sync_group_index_task.log'));
+        $schedule->command('sync_user_index_task')->cron('0 */23 * * *')->appendOutputTo( storage_path('logs/sync_user_index_task.log'));
+        $schedule->command('sync_article_index_task')->cron('0 */27 * * *')->appendOutputTo( storage_path('logs/sync_question_index_task.log'));
+        $schedule->command('sync_question_index_task')->everyMinute()->appendOutputTo( storage_path('logs/sync_question_index_task.log'));
+        $schedule->command('sync_course_score_task')->cron('0 */31 * * *')->appendOutputTo( storage_path('logs/sync_course_score_task.log'));
+        $schedule->command('sync_article_score_task')->cron('0 */33 * * *')->appendOutputTo( storage_path('logs/sync_article_score_task.log'));
+        $schedule->command('sync_question_score_task')->cron('0 */37 * * *')->appendOutputTo( storage_path('logs/sync_question_score_task.log'));
+        $schedule->command('clean_log_task')->dailyAt('03:03')->appendOutputTo( storage_path('logs/clean_log_task.log'));
+        $schedule->command('unlock_user_task')->dailyAt('03:07')->appendOutputTo( storage_path('logs/unlock_user_task.log'));
+        $schedule->command('revoke_vip_task')->dailyAt('03:11')->appendOutputTo( storage_path('logs/revoke_vip_task.log'));
+//        $schedule->command('sync_app_info_task')->dailyAt('03:13')->appendOutputTo( storage_path('logs/sync_app_info_task.log'));
+        $schedule->command('sync_tag_stat_task')->dailyAt('03:17')->appendOutputTo( storage_path('logs/sync_tag_stat_task.log'));
+        $schedule->command('sync_course_stat_task')->dailyAt('03:19')->appendOutputTo( storage_path('logs/sync_course_stat_task.log'));
+        $schedule->command('close_question_task')->dailyAt('03:23')->appendOutputTo( storage_path('logs/close_question_task.log'));
+//        $schedule->command('sitemap_task')->dailyAt('04:03');
+//        $schedule->command('teacher_live_notice_task')->dailyAt('04:07');
     }
 
     /**
